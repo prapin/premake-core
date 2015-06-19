7fa87667d9a4ea3abaf5eea70b2e7c5e0a5d5889
Patrick Rapin <patrick@xample.ch>
2015-06-19 15:10:13 +0200

    Only overwrite an existing project file if the content is different than the existing one. This prevents useless reloading in IDEs.

--- src/base/io.lua
+++ src/base/io.lua
@@ -10,13 +10,43 @@
 -- subdirectories in the filename if "mode" is set to writeable.
 --
 
+	local file2name = {}
+	
+	local function readFile(filename)
+		local f = io.open(filename, "rb");
+		if f == nil then return end
+		local content = f:read("*a");
+		f:close()
+		return content
+	end
+
+	premake.override(getmetatable(io.stdout), "close", function(base, file)
+		base(file)
+		local name = file2name[file]
+		if not name then return end
+		file2name[file] = nil
+		local tmpName = name .. ".tmp"
+		local orig = readFile(name)
+		local newer = readFile(tmpName)
+		if orig == newer then
+			os.remove(tmpName)
+		else
+			os.remove(name)
+			os.rename(tmpName, name)
+		end
+	end)
+
+
 	premake.override(io, "open", function(base, fname, mode)
-		if mode and (mode:find("w") or mode:find("a"))  then
+		if mode and mode:find("w") then
 			local dir = path.getdirectory(fname)
 			ok, err = os.mkdir(dir)
 			if not ok then
 				error(err, 0)
 			end
+			local file = base(fname .. ".tmp", mode)
+			file2name[file] = fname
+			return file
 		end
 		return base(fname, mode)
 	end)
